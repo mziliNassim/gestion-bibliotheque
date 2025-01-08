0a6747a77d00502203e5a6141f21b2f006008348
mziliNassim <mzilinassim@gmail.com>
2025-01-08 17:20:17 +0100

    Livres

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Express App Initialization
 const app = express();
-dotenv.config();
 
+// Middlewares
 app.use(express.json());
+dotenv.config();
 
 // "!don" : routes
 app.use("/auth", require("./routes/auth.route"));
+app.use("/livres", require("./routes/livres.route"));
 
 // "!To do" : routes
-app.use("/livres", require("./routes/livres.route"));
 app.use("/emprunts", require("./routes/emprunts.route"));
 app.use("/clients", require("./routes/clients.route"));
 app.use("/notifications", require("./routes/notifications.route"));
 app.use("/payiement", require("./routes/payiement.route"));
 
+// Server Listening
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server on http://localhost:${port}/`);
--- controllers/livres.controller.js
+++ controllers/livres.controller.js
@@ -1,7 +1,7 @@
 const dbConnection = require("../config/db");
 const { ObjectId } = require("mongodb");
 
-// getLivres controller
+// Get Livres - controller
 const getLivres = async (req, res) => {
   try {
     const db = await dbConnection();
@@ -15,7 +15,7 @@ const getLivres = async (req, res) => {
   }
 };
 
-// getLivres controller
+// Get Livre By Id -  controller
 const getLivresById = async (req, res) => {
   try {
     const id = req.params.id;
@@ -33,40 +33,96 @@ const getLivresById = async (req, res) => {
       .status(200)
       .json({ message: `Livres pour  id : ${id}`, data: result });
   } catch (error) {
-    console.log("getLivresById ~ error:", error);
+    console.log("getLivresById ~ error:", error.message);
     return res
       .status(500)
       .json({ message: "Impossible de récupérer le liver", data: null });
   }
 };
 
-// getLivres controller
+// Post Livre - controller
 const addLivre = async (req, res) => {
   try {
     const newLivre = req.body;
     const db = await dbConnection();
     const result = await db.collection("Livres").insertOne(newLivre);
-    res.status(200).json({
+    return res.status(200).json({
       message: "Livre added successfuly!",
       data: { _id: result.insertedId, ...newLivre },
     });
   } catch (err) {
     console.log("addLivre ~ err:", err.message);
-    res.status(500).json({
+    return res.status(500).json({
       message: `Impossible de créer l'élément`,
       data: null,
     });
   }
 };
 
-// getLivres controller
+// Put Livre - controller
 const updateLivre = async (req, res) => {
-  res.status(200).json({ message: "success" });
+  try {
+    const id = req.params.id;
+    const updatedLivre = req.body;
+    const db = await dbConnection();
+    const result = await db
+      .collection("Livres")
+      .updateOne({ _id: new ObjectId(id) }, { $set: updatedLivre });
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({
+        message: "Livre not found.",
+        data: null,
+      });
+    }
+
+    if (result.modifiedCount === 0) {
+      return res.status(200).json({
+        message: "No changes made to the Livre.",
+        data: updatedLivre,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Livre updated successfully!",
+      data: { _id: id, ...updatedLivre },
+    });
+  } catch (error) {
+    console.log("updateLivre ~ error:", error.message);
+    return res.status(500).json({
+      message: `Impossible de créer l'élément`,
+      data: null,
+    });
+  }
 };
 
-// getLivres controller
+// delete Livre - controller
 const deleteLivre = async (req, res) => {
-  res.status(200).json({ message: "success" });
+  try {
+    const id = req.params.id;
+    const db = await dbConnection();
+    const result = await db
+      .collection("Livres")
+      .deleteOne({ _id: new ObjectId(id) });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({
+        message: "Livre not found.",
+        data: null,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Livre deleted successfully!",
+      data: null,
+    });
+  } catch (error) {
+    console.log("deleteLivre ~ error:", error.message);
+    return res.status(500).json({
+      message: "Impossible de supprimer l'élément.",
+      data: null,
+    });
+  }
 };
 
 module.exports = {
